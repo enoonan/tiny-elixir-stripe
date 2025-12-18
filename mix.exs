2cd45d357fc9697f97e57b0acb0e12cb3450e01b
Eileen Noonan <enoonan@arcstone.com>
2025-12-18 17:24:52 -0500

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule TinyElixirStripe.MixProject do
   use Mix.Project
 
-  @version "0.0.3"
+  @version "0.0.5"
   @source_url "https://github.com/enoonan/tiny-elixir-stripe"
 
   def project do
